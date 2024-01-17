93b0ad5af900bdb9b5d9435f9fc8191e3eb9fe7b
cmoonth <cmoonth47@gmail.com>
2024-01-17 17:32:55 +0900

    24/01/17 practice Scanner class

--- src/scanner/ex/ChangeVarEx.java
+++ src/scanner/ex/ChangeVarEx.java
@@ -1,7 +1,6 @@
 package scanner.ex;
 
 public class ChangeVarEx {
-
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,32 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요: ");
+
+        int sum = 0;
+//        double avg = 0;
+        int count = 0;
+        while (true) {
+            int number = scanner.nextInt();
+            if (number == -1) {
+//                System.out.println("입력한 숫자들의 합계: " + sum);
+//                System.out.println("입력한 숫자들의 평균: " + avg);
+                break;
+            }
+//            count++;
+            sum = sum + number;
+            count++;
+//            avg = (double) sum /count;
+        }
+
+        double avg = (double) sum / count;
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + avg);
+    }
+}
